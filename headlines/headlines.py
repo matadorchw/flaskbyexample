94a16ad26af5dc18af65e26b6822fe36b9289cc7
truman <2290350531@qq.com>
2020-10-22 00:04:20 +0800

    chap2 : show one

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,11 +1,27 @@
+import feedparser
 from flask import Flask
 
 app = Flask(__name__)
 
+ZHIHU_FEED = 'https://www.zhihu.com/rss'
+
 
 @app.route('/')
 def get_news():
-    return 'no news is good news'
+    feed = feedparser.parse(ZHIHU_FEED)
+    first_article = feed['entries'][0]
+    return '''
+<html>
+    <body>
+        <h1>ZhiHu Headlines</h1>
+        <b>{0}</b><br/>
+        <i>{1}</i><br/>
+        <p>{2}</p><br/>
+    </body>
+</html>
+    '''.format(first_article.get('title'),
+               first_article.get('published'),
+               first_article.get('summary'))
 
 
 if __name__ == '__main__':
