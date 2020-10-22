7945d16d1fc1d7ebd8b9b1da867514f5018cb56c
truman <2290350531@qq.com>
2020-10-22 11:38:35 +0800

    chap2: feeds

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -3,17 +3,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
-ZHIHU_FEED = 'https://www.zhihu.com/rss'
+RSS_FEEDS = {
+    'zhihu': 'https://www.zhihu.com/rss',
+    'read': 'http://feed.read.org.cn/',
+    '163': 'https://news.163.com/special/00011K6L/rss_newsattitude.xml'
+}
 
 
 @app.route('/')
-def get_news():
-    feed = feedparser.parse(ZHIHU_FEED)
+@app.route('/163')
+def news163():
+    return get_news('163')
+
+
+@app.route('/read')
+def read():
+    return get_news('read')
+
+
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
     return '''
 <html>
     <body>
-        <h1>ZhiHu Headlines</h1>
+        <h1>Headlines</h1>
         <b>{0}</b><br/>
         <i>{1}</i><br/>
         <p>{2}</p><br/>
@@ -25,4 +39,4 @@ def get_news():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
