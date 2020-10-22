d3bb7a4b7470b2271b7e9e57812473e414fc7710
truman <2290350531@qq.com>
2020-10-22 17:00:13 +0800

    chap4: display weather data

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,5 +1,8 @@
 import feedparser
 from flask import Flask, render_template, request
+import json
+import urllib.request
+import urllib.parse
 
 app = Flask(__name__)
 
@@ -17,7 +20,25 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template('home.html', articles=feed['entries'])
+    weather = get_weather('Shenzhen,CN')
+    return render_template('home.html', articles=feed['entries'],
+                           weather=weather)
+
+
+def get_weather(query):
+    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=cb932829eacb6a0e9ee4f38bfbf112ed'
+    query = urllib.parse.quote(query)
+    url = api_url.format(query)
+    data = urllib.request.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get('weather'):
+        weather = {
+            'description': parsed['weather'][0]['description'],
+            'temperature': parsed['main']['temp'],
+            'city': parsed['name']
+        }
+    return weather
 
 
 if __name__ == '__main__':
