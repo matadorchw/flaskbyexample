449feb2a8705242705a9e583b259a820e11f6036
truman <2290350531@qq.com>
2020-10-24 19:36:59 +0800

    chap7: add new crimes functionality

--- crimemap/crimemap.py
+++ crimemap/crimemap.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, render_template, request
 
 import dbconfig
@@ -13,12 +14,9 @@ DB = DBHelper()
 
 @app.route('/')
 def home():
-    try:
-        data = DB.get_all_inputs()
-    except Exception as e:
-        print(e)
-        data = None
-    return render_template('home.html', data=data,
+    crimes = DB.get_all_crimes()
+    crimes = json.dumps(crimes)
+    return render_template('home.html', crimes=crimes,
                            map_api_key=dbconfig.map_api_key)
 
 
@@ -38,6 +36,17 @@ def clear():
         DB.clear_all()
     except Exception as e:
         print(e)
+    return home()
+
+
+@app.route('/submitcrime', methods=['POST'])
+def submitcrime():
+    category = request.form.get('category')
+    date = request.form.get('date')
+    latitude = float(request.form.get('latitude'))
+    longitude = float(request.form.get('longitude'))
+    description = request.form.get('description')
+    DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
 
--- crimemap/mockdbhelper.py
+++ crimemap/mockdbhelper.py
@@ -10,3 +10,23 @@ class MockDBHelper:
 
     def clear_all(self):
         pass
+
+    def add_crime(self, category, date, latitude, longitude, description):
+        pass
+
+    def get_all_crimes(self):
+        return [{
+            'latitude': 30.604,
+            'longitude': 113.247,
+            'date': '2020-10-23',
+            'category': 'mugging',
+            'description': 'mock description'
+        },
+            {
+                'latitude': 30.604,
+                'longitude': 113.347,
+                'date': '2020-10-24',
+                'category': 'mugging',
+                'description': 'mock description'
+            }
+        ]
